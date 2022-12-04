c76e613659aa9f0162cd4bdd2fe93a28a8c8ab07
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-04 12:07:28 -0600

    practicamos POO desde leccion 6 suma

--- Java Intermedio/src/video6_POO_Suma/Main.java
+++ Java Intermedio/src/video6_POO_Suma/Main.java
@@ -1,21 +1,20 @@
-
 package video6_POO_Suma;
 
 import java.util.Scanner;
 
-public class Main {              
-       
-    public static void main(String[] args) {
-         
+public class Main{
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         
-        System.out.println("Ingrese su primer sifra");
-         int  num1 = in.nextInt();
-         
-         System.out.println("Ingrese el segundo numero");
-         int num2 = in.nextInt();
-         
-         Suma instancia = new Suma(num1,num2);
-         instancia.Resultado();
+        System.out.println("Ingrese si primer valor");
+        int num1 = in.nextInt();
+        
+        System.out.println("Ingrese su segunda sifra");
+        int num2 = in.nextInt();
+        
+        Suma objeto = new Suma(num1, num2);
+        objeto.Operacion();
+        objeto.Resultado();
+        
     }
 }
--- Java Intermedio/src/video6_POO_Suma/Suma.java
+++ Java Intermedio/src/video6_POO_Suma/Suma.java
@@ -1,20 +1,18 @@
 package video6_POO_Suma;
 
-public class Suma {
-   private int num1,num2,resultado;
-   
-    public Suma(int num1,int num2){
+
+public class Suma{
+    private int num1, num2, resultado;
+    
+    public Suma(int num1, int num2){
         this.num1 = num1;
-       this.num2 = num2;
-        
-       
+        this.num2 = num2;
     }
-    public void Operaciones(){
-        resultado = num1+num2;
+    public void Operacion(){
+        resultado = num1 +num2;
     }
     public void Resultado(){
-        Operaciones();
-        System.out.println("El resultado de su suma es: "+ resultado);
+        //Operacion();
+        System.out.println("El resultado de tu suma es:" + resultado);
     }
-    
 }
