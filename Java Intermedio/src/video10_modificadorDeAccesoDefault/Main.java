30dee7a583702e9bdb09bd14b264f9a3c49f5873
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-04 20:02:02 -0600

    agreagmos video 10

---
+++ Java Intermedio/src/video10_modificadorDeAccesoDefault/Main.java
@@ -0,0 +1,11 @@
+
+package video10_modificadorDeAccesoDefault;
+
+public class Main {
+    
+    /*los modificadores de acceso "default" son los que vienen po defecto cuando declaramos 
+    alguna variable y su prinsipal caracteristica esque  solo se pueden accesar a las variables
+    o metodos desde su mismo paquete a si  que si queremos traer desde otra clase en otro paquete
+    una variable que tenga el modificador de acceso "default" no se podra*/
+    
+}
--- Java Intermedio/src/video9_POO_lavadora/LavadoraMain.java
+++ Java Intermedio/src/video9_POO_lavadora/LavadoraMain.java
@@ -1,28 +1,27 @@
 package video9_POO_lavadora;
+
 import video8_POO_lavadora.Funciones;
 import java.util.Scanner;
 
 public class LavadoraMain {
-    
-   
-        public static void main(String[] args) {
-            
-             Scanner in = new Scanner(System.in);
-            int tipRopa,kilos;
-    
-            System.out.println("Bienvenido a la lavadora Samsung\n");
-            System.out.println("Que tipo de Ropa desea lavar");            
-            System.out.println("1.- Ropa de Color  / lavado Rustico ");
-            System.out.println("2.- Ropa Blancla / lavado suave");
-            tipRopa = in.nextInt();
-            System.out.println("Cuantos kilos desea lavar? maximo 12K");
-            kilos = in.nextInt();
-            
-            
-            Funciones instancia = new Funciones(tipRopa, kilos);
-            instancia.setTipRopa(2);
-            System.out.println("el tipo de ropa establecido por default " + instancia.getTipRopa());
-            instancia.SicloFinalizado();
-            
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int tipRopa, kilos;
+
+        System.out.println("Bienvenido a la lavadora Samsung\n");
+        System.out.println("Que tipo de Ropa desea lavar");
+        System.out.println("1.- Ropa de Color  / lavado Rustico ");
+        System.out.println("2.- Ropa Blancla / lavado suave");
+        tipRopa = in.nextInt();
+        System.out.println("Cuantos kilos desea lavar? maximo 12K");
+        kilos = in.nextInt();
+
+        Funciones instancia = new Funciones(tipRopa, kilos);
+        instancia.setTipRopa(2);
+        System.out.println("el tipo de ropa establecido por default " + instancia.getTipRopa());
+        instancia.SicloFinalizado();
+
     }
 }
