537ab991aa6977086d18c6a6499a391e51146843
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-04 20:16:37 -0600

    subimos video 12

---
+++ Java Intermedio/src/video12_modificadorpublic/Main.java
@@ -0,0 +1,14 @@
+
+package video12_modificadorpublic;
+
+/**
+ *
+ * @author Emman
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*el modificador de acceso publico es un tipo de encapsulaminto de elemntos
+        ya sean variables metodos o atributos, permite acceder al elemto desde cualquier clase
+        independientemente que este se encuentre o no en le mismo paquete.*/
+    }
+}
