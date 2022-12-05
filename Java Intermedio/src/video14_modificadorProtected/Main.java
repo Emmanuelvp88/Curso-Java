42be2e02886957ff27697743889489342a70ac27
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-04 20:32:11 -0600

    agregamos video 14

---
+++ Java Intermedio/src/video14_modificadorProtected/Main.java
@@ -0,0 +1,15 @@
+
+package video14_modificadorProtected;
+
+/**
+ *
+ * @author Emman
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*el modificador "protected" permite que lso elementos solo pueden ser
+        accedidos desde su mismo paquete(al igual que el modificador "default")
+        y tambien desde cualquier clase que herede la clase en donde se encuentran
+        independientemente si las clases se encuentran en el mismo paquete o no*/
+    }
+}
